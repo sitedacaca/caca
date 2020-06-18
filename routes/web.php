160a5fbfae380d48c3973b1e00fa1d814b26d111
Leonardo <leosampsousa@gmail.com>
2020-06-18 00:19:51 -0300

    MANUTENCAO

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('manutencao');
+    return view('home');
 })->name('home');
 
 Route::get('/Sobre', function () {
